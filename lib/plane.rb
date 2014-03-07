4d53ba3fa8f559f900544ede7801efa7f9e2a5d8
Mikhail Dubov <mdubov@gmail.com>
2014-03-07 12:35:48 +0000

    Added some basic plane behaviour

---
+++ lib/plane.rb
@@ -0,0 +1,19 @@
+class Plane
+  # attr_accessor :flying?
+
+  def initialize
+    @flying = true
+  end
+
+  def flying?
+    @flying
+  end
+
+  def land
+    @flying = false
+  end
+
+  def take_off
+    @flying = true
+  end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,26 @@
+require 'plane'
+
+describe Plane do
+ 
+  let(:plane) { Plane.new }
+  
+  it 'has a flying status when created' do
+    expect(plane).to be_flying
+  end
+  
+  it 'has a flying status only when in the air' do
+    plane.land
+    expect(plane).not_to be_flying
+  end
+  
+  it 'can take off' do
+    
+    expect(plane).to respond_to(:take_off)
+  end
+  
+  it 'changes its status to flying after taking of' do
+    plane.land
+    plane.take_off
+    expect(plane).to be_flying
+  end
+end
